bd3609e090651d00a2440b60e19b390eb157219c
jamesbchurch <jamesbchurch@outlook.com>
2022-12-12 20:28:06 +1000

    Built out Hero Slice
    Built out hero slice, need to adjust mobile and tablet views and add in correct font.

--- design/src/components/HeroSlice.js
+++ design/src/components/HeroSlice.js
@@ -1,7 +1,22 @@
+import styles from "./HeroSlice.module.css";
+
 export default function HeroSlice() {
   return (
-    <div>
-      <img src="image-amy.webp" alt="amy" />
+    <div className={styles.flexContainer}>
+      <img src="image-amy.webp" alt="amy" width="350" height="350" />
+      <span className={styles.ctaFlex}>
+        <h2 className={styles.headline}>
+          I’m Amy, and I’d love to work on your next project
+        </h2>
+
+        <p className={styles.description}>
+          I love working with others to create beautiful design solutions. I’ve
+          designed everything from brand illustrations to complete mobile apps.
+          I’m also handy with a camera!{" "}
+        </p>
+
+        <button className={styles.button}>Free Consultation </button>
+      </span>
     </div>
   );
 }
