45eb2af5a089a157057df72ac3704dd550573f76
jamesbchurch <jamesbchurch@outlook.com>
2022-12-12 19:02:07 +1000

    First commit

---
+++ design/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import HeroSlice from "./components/HeroSlice";
+
+function App() {
+  return (
+    <div className="App">
+      <HeroSlice />
+    </div>
+  );
+}
+
+export default App;
---
+++ design/src/components/HeroSlice.js
@@ -0,0 +1,7 @@
+export default function HeroSlice() {
+  return (
+    <div>
+      <img src="image-amy.webp" alt="amy" />
+    </div>
+  );
+}
