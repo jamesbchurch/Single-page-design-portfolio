134cdb48aeb4ed3efc1701d4f0ca6cc7dfd71dec
jamesbchurch <jamesbchurch@outlook.com>
2022-12-15 05:46:39 +1000

    Added in text for grid
    Added in text for the grid.

--- design/src/components/ServicesSlice/ServicesSlice.js
+++ design/src/components/ServicesSlice/ServicesSlice.js
@@ -9,21 +9,27 @@ export default function ServicesSlice() {
           src="pattern-graphic-design.svg"
           alt="graphics"
         />
+        <h2 className={styles.graphicsText}>Graphic Design</h2>
       </li>
       <li className={styles.ui}>
         <img src="pattern-ui-ux.svg" alt="ui" />
+        <h2>UI/UX</h2>
       </li>
       <li className={styles.apps}>
         <img src="pattern-apps.svg" alt="apps" />
+        <h2>Apps</h2>
       </li>
       <li className={styles.spanRow}>
         <img src="pattern-illustrations.svg" alt="illustrations" />
+        <h2>Illustrations</h2>
       </li>
       <li className={styles.spanRow}>
         <img src="pattern-photography.svg" alt="photos" />
+        <h2>Photography</h2>
       </li>
       <li className={styles.spanRow}>
         <img src="pattern-motion-graphics.svg" alt="motion" />
+        <h2>Motion Graphics</h2>
       </li>
     </ul>
   );
