a4b9e675c4819ec4370f1aebe70a0dc255878121
jamesbchurch <jamesbchurch@outlook.com>
2022-12-23 06:04:36 +1000

    Built out the call slice
    Updated the call slice and adjusted this for Mobile, Tablet and Desktop sizes.

--- design/src/components/ContactSlice/ContactSlice.js
+++ design/src/components/ContactSlice/ContactSlice.js
@@ -4,12 +4,14 @@ import Button from "../Button/Button";
 export default function ContactSlice() {
   return (
     <div className={styles.container}>
-      <h2>Book a call with me</h2>
-      <p>
-        I love working with others to create beautiful design solutions. I’ve
-        designed everything from brand illustrations to complete mobile apps.
-        I’m also handy with a camera!
-      </p>
+      <div className={styles.content}>
+        <h2 className={styles.title}>Book a call with me</h2>
+        <p className={styles.description}>
+          I love working with others to create beautiful design solutions. I’ve
+          designed everything from brand illustrations to complete mobile apps.
+          I’m also handy with a camera!
+        </p>
+      </div>
       <Button
         text={"Free Consultation"}
         colour={"#eb7565"}
--- design/src/components/HeroSlice/HeroSlice.js
+++ design/src/components/HeroSlice/HeroSlice.js
@@ -26,3 +26,4 @@ export default function HeroSlice() {
     </div>
   );
 }
+
