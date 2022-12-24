57028da4ec1d197e80ea1cc31b942c835a887022
jamesbchurch <jamesbchurch@outlook.com>
2022-12-25 05:47:59 +1000

    Slider updated
    Build out slider carousel with images. Todo, adjust for mobile and tablet and adjust buttons.

--- design/src/components/CarouselSlice/CarouselSlice.js
+++ design/src/components/CarouselSlice/CarouselSlice.js
@@ -1,17 +1,39 @@
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 export default function CarouselSlice() {
   return (
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={1}
-      onSlideChange={() => console.log("slide change")}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide>1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-    </Swiper>
+    <div>
+      <Swiper
+        modules={{ Navigation, Pagination }}
+        spaceBetween={0}
+        slidesPerView={3}
+        navigation
+        pagination={{ clickable: true }}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
+          <img src="/image-slide-1.jpg" width={540} height={360} alt="slide" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/image-slide-2.jpg" width={540} height={360} alt="slide" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/image-slide-3.jpg" width={540} height={360} alt="slide" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/image-slide-4.jpg" width={540} height={360} alt="slide" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/image-slide-5.jpg" width={540} height={360} alt="slide" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
   );
 }
