77f79be0d953f46f7b9f21c1535c5a8450585fe5
jamesbchurch <jamesbchurch@outlook.com>
2022-12-26 11:51:43 +1000

    Added header slice
    Todo: Update styling across site and adjust swiper js to work correctly with the buttons. And style and move these buttons outside the swiper.

--- design/src/App.js
+++ design/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import CarouselSlice from "./components/CarouselSlice/CarouselSlice";
 import ContactSlice from "./components/ContactSlice/ContactSlice";
+import HeadingSlice from "./components/HeadingSlice/HeadingSlice";
 import HeroSlice from "./components/HeroSlice/HeroSlice";
 import NavSlice from "./components/NavSlice/NavSlice";
 import ServicesSlice from "./components/ServicesSlice/ServicesSlice";
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <NavSlice />
+      <HeadingSlice />
       <ServicesSlice />
       <HeroSlice />
       <CarouselSlice />
---
+++ design/src/components/HeadingSlice/HeadingSlice.js
@@ -0,0 +1,13 @@
+import styles from "./HeadingSlice.module.css";
+
+export default function HeadingSlice() {
+  return (
+    <div className={styles.container}>
+      <h1>Design solutions made easy</h1>
+      <p>
+        With over ten years of experience in various design disciplines, I’m
+        your one-stop shop for your design needs.
+      </p>
+    </div>
+  );
+}
